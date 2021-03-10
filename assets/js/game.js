533d4fe8c4d6f7beceec7443b007669e875c0d1b
bubsy07 <bubsy_07@HOTMAIL.CO.UK>
2021-03-10 20:39:13 +0000

    initial commit

---
+++ assets/js/game.js
@@ -0,0 +1,7 @@
+ //Plays background music, repeats on song end
+   window.addEventListener("DOMContentLoaded", event => {
+  const audio = document.querySelector("audio");
+  audio.volume = 0.2;
+  audio.play();
+});
+

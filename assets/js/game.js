33b10d39a5d41bff59fb4409f2ede42b7432f6f5
bubsy07 <bubsy_07@HOTMAIL.CO.UK>
2021-03-12 17:30:14 +0000

    overlay mod

--- assets/js/game.js
+++ assets/js/game.js
@@ -5,3 +5,39 @@
   audio.play();
 });
 
+
+
+//Overlay page
+
+//turn links colour on hover
+
+ $("#start").mouseenter(function() {
+        $(this).css('color', 'lightgreen');
+   }); 
+
+$("#start").mouseleave(function() {
+        $(this).css('color', 'azure');
+   });
+
+$("#how-to-play").mouseenter(function() {
+        $(this).css('color', 'lightgreen');
+   }); 
+
+$("#how-to-play").mouseleave(function() {
+        $(this).css('color', 'azure');
+   });
+
+
+//Starts the game when 'Click to Start' is clicked on landing overlay
+        
+$(document).ready(function(){
+  $("#start").click(function(){
+    $("#front-page").hide();
+  });
+});
+
+   //Opens game instructions
+    $("#how-to-play").click(function () {
+        $("#instructions").slideToggle(1000);
+        
+    });

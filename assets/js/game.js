719e25018c7002e129885a2f39e1f3692a27f1a2
bubsy07 <bubsy_07@HOTMAIL.CO.UK>
2021-03-13 20:57:38 +0000

    13/03

--- assets/js/game.js
+++ assets/js/game.js
@@ -27,17 +27,56 @@ $("#rules-of-game").mouseleave(function() {
         $(this).css('color', 'azure');
    });
 
-
-//Starts the game when 'Click to Start' is clicked on landing overlay
-        
+  
+//Opens game instructions on front page
 $(document).ready(function(){
-  $("#start-game").click(function(){
+    $("#game-rules").hide();    
+    $("#rules-of-game").click(function () {
+    $("#game-rules").slideToggle(1000);
+        
+    });
+});
+
+//Starts the game when 'Click to Play Game' is clicked on front page
+$(document).ready(function(){
+  
+    $("#start-game").click(function(){
     $("#front-page").hide();
+    game.startGame;
+   
   });
 });
 
-   //Opens game instructions
-    $("#rules-of-game").click(function () {
-        $("#game-rules").slideToggle(1000);
-        
-    });
+   //Starts the timer when game is started 
+
+    function startGame() {
+    var fiveMinutes = 60 * 5,
+        display = document.querySelector('#time');
+    startTimer(fiveMinutes, display);
+};
+
+   function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10)
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
+
+
+
+
+
+   
+
+  
+ 

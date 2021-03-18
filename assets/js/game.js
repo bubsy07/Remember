7ad8f4da3761bfa51e50908a5c0c322f66f9be00
bubsy07 <bubsy_07@HOTMAIL.CO.UK>
2021-03-18 14:17:11 +0000

    18-03

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,8 +1,8 @@
- //Plays background music, repeats on song end
-   window.addEventListener("DOMContentLoaded", event => {
-  const audio = document.querySelector("audio");
-  audio.volume = 0.2;
-  audio.play();
+//Plays background music, repeats on song end
+window.addEventListener("DOMContentLoaded", event => {
+    const audio = document.querySelector("audio");
+    audio.volume = 0.2;
+    audio.play();
 });
 
 
@@ -11,110 +11,110 @@
 
 //turn links colour on hover
 
- $("#start-game").mouseenter(function() {
-        $(this).css('color', 'lightgreen');
-   }); 
+$("#start-game").mouseenter(function () {
+    $(this).css('color', 'lightgreen');
+});
 
-$("#start-game").mouseleave(function() {
-        $(this).css('color', 'azure');
-   });
+$("#start-game").mouseleave(function () {
+    $(this).css('color', 'azure');
+});
 
-$("#rules-of-game").mouseenter(function() {
-        $(this).css('color', 'lightgreen');
-   }); 
+$("#rules-of-game").mouseenter(function () {
+    $(this).css('color', 'lightgreen');
+});
 
-$("#rules-of-game").mouseleave(function() {
-        $(this).css('color', 'azure');
-   });
-$("#game-over-restart").mouseenter(function() {
-        $(this).css('color', 'lightgreen');
-   }); 
+$("#rules-of-game").mouseleave(function () {
+    $(this).css('color', 'azure');
+});
+$("#game-over-restart").mouseenter(function () {
+    $(this).css('color', 'lightgreen');
+});
 
-$("#game-over-restart").mouseleave(function() {
-        $(this).css('color', 'azure');
-   });
+$("#game-over-restart").mouseleave(function () {
+    $(this).css('color', 'azure');
+});
 
 
-  
+
 //Opens game instructions on front page
-$(document).ready(function(){
-    $("#game-rules").hide();    
+$(document).ready(function () {
+    $("#game-rules").hide();
     $("#rules-of-game").click(function () {
-    $("#game-rules").slideToggle(1000);        
+        $("#game-rules").slideToggle(1000);
     });
 });
 
 
 //Starts the game when 'Click to Play Game' is clicked on front page
-$(document).ready(function(){
-  
+$(document).ready(function () {
+
     $("#start-game").click(startGame);
-    
-  });
+
+});
 
 
-   //Starts the timer when game is started - hide front page
+//Starts the timer when game is started - hide front page
 
-    function startGame() {
-        console.log("starting game")
+function startGame() {
+    console.log("starting game")
     var fiveMinutes = 60 * 1,
         display = document.querySelector('#time');
     startTimer(fiveMinutes, display);
 
     $("#front-page").hide();
-    
+
 };
 
-   function startTimer(duration, display)  {
-  let counter = duration;
-    
-  const interval = setInterval(() => {
-    console.log(counter);
-    counter--;
-      
-    display.textContent = counter;
-    if (counter === 0 ) {
-      clearInterval(interval);    
-      gameOver();
-    }
-  }, 1000);
+function startTimer(duration, display) {
+    let counter = duration;
+
+    const interval = setInterval(() => {
+        console.log(counter);
+        counter--;
+
+        display.textContent = counter;
+        if (counter === 0) {
+            clearInterval(interval);
+            gameOver();
+        }
+    }, 1000);
 }
 
 //Game Over Function called when you timer expires
 //add the overlay to the game-over-page in line 81. But we set the CSS of game-over-page to display: none, 
 //so they will not show unless we set the display back:
 
-    function gameOver() {
-        let el =  document.getElementById("game-over-page")    
-        el.classList.add("overlay");
-        el.style.display = "initial";     
+function gameOver() {
+    let el = document.getElementById("game-over-page")
+    el.classList.add("overlay");
+    el.style.display = "initial";
 }
-   
+
 //Game restart Function - hide game end over lay - start game
 
- $("#game-over-restart").click(function() {
-$("#game-over-page").hide();
-startGame();
+$("#game-over-restart").click(function () {
+    $("#game-over-page").hide();
+    startGame();
 });
 
 
- //Game Function - image click - box color
+//Game Function - image click - box color
 
 let box = document.getElementsByClassName('box');
 
 let l = box.length;
-for(let i = 0;  i < l; i++) {
+for (let i = 0; i < l; i++) {
     box[i].addEventListener('click', imgClicked);
 }
 
-
 function imgClicked() {
-$(this).children.classList.add("layer");
+    console.log(this)
+    console.log(this.parentElement)
+    $(this).parentElement().addClass("layer");
 
 
 
+    console.log()
 
-console.log(this)
-
-   console.log("clicked")
+    console.log("clicked")
 }

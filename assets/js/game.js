3923cbc9948173b527668e6bc06b5e12fc143b72
bubsy07 <bubsy_07@HOTMAIL.CO.UK>
2021-03-27 11:06:03 +0000

    clicks_styles

--- assets/js/game.js
+++ assets/js/game.js
@@ -5,8 +5,7 @@ window.addEventListener("DOMContentLoaded", event => {
     audio.play();
 });
 
-
-//Overlay page
+//Overlay pages
 
 //turn links colour on hover
 
@@ -25,119 +24,69 @@ $("#rules-of-game").mouseenter(function () {
 $("#rules-of-game").mouseleave(function () {
     $(this).css('color', 'azure');
 });
-$("#game-over-restart").mouseenter(function () {
+
+$("#back-to-home").mouseenter(function () {
     $(this).css('color', 'lightgreen');
 });
 
-$("#game-over-restart").mouseleave(function () {
+$("#back-to-home").mouseleave(function () {
+    $(this).css('color', 'azure');
+});
+
+$("#win-home").mouseenter(function () {
+    $(this).css('color', 'lightgreen');
+});
+
+$("#win-home").mouseleave(function () {
+    $(this).css('color', 'azure');
+});
+
+$("#contact").mouseenter(function () {
+    $(this).css('color', 'lightgreen');
+});
+
+$("#contact").mouseleave(function () {
     $(this).css('color', 'azure');
 });
 
 
-//Opens game instructions on front page
 $(document).ready(function () {
-    $("#game-rules").hide();
+    //Create Array of 'card'
+    let cards = Array.from(document.getElementsByClassName('card'));
+
+    //time/card array
+    let play = new remember(cards, 60);
+
+    //Start on front page
+
+$('#start-game').click(function () {
+    $("#front-page").hide();
+        
+        play.startRemember();
+    });
+
+    //Rules of the game
     $("#rules-of-game").click(function () {
-        $("#game-rules").slideToggle(1000);
+        $("#what-to-do").slideToggle("slow"); 
+        $("#contact-form").hide("slow");   
+    });
+
+    //Reload game 
+    $('#back-to-home, #win-home').click(function () {
+        location.reload();
+    });
+
+    //contact
+    $("#contact").click(function () {
+        $("#contact-form").slideToggle("slow"); 
+        $("#what-to-do").hide("slow");  
+    });
+
+    //Flips the cards on click
+    cards.forEach(card => {
+        card.addEventListener('click', () => {
+            play.flipCard(card);
+        });
     });
 });
 
-
-
-//Starts the game when 'Click to Play Game' is clicked on front page
-$(document).ready(function () {
-
-    $("#start-game").click(startGame);
-
-});
-
-//Starts the timer when game is started - hide front page
-
-function startGame() {
-    console.log("starting game")
-    var fiveMinutes = 60 * 1,
-        display = document.querySelector('#time');
-    startTimer(fiveMinutes, display);
-
-    $("#front-page").hide();
-    shuffleCards(cardArray);
-   
-    trial = document.getElementsByClassName('text');
-    trial.style.visibility = "hidden"
-    
-
-};
-
-function startTimer(duration, display) {
-    let counter = duration;
-
-    const interval = setInterval(() => {
-        console.log(counter);
-        counter--;
-
-        display.textContent = counter;
-        if (counter === 0) {
-            clearInterval(interval);
-            gameOver();
-        }
-    }, 1000);
-}
-
-//Game Over Function called when you timer expires
-//add the overlay to the game-over-page in line 81. But we set the CSS of game-over-page to display: none, 
-//so they will not show unless we set the display back:
-
-function gameOver() {
-    let el = document.getElementById("game-over-page")
-    el.classList.add("overlay");
-    el.style.display = "initial";
-    
-    
-}
-
-//Game restart Function - hide game end over lay - start game
-
-$("#game-over-restart").click(function () {
-    $("#game-over-page").hide();
-    startGame();
-});
-
-
-//Game Function - image click - box
-
-let box = document.getElementsByClassName('box');
-
-let l = box.length;
-for (let i = 0; i < l; i++) {
-    box[i].addEventListener('click', imgClicked);
-}
-
-function imgClicked() {
-    console.log(this)
-    console.log(this.parentElement)
-
-
- this.nextElementSibling.style.visibility = "visible"
-
-    console.log(this.nextElementSibling)
-    console.log("clicked")
-}
-
-
-//Create Array from all elements with the 'picture' class
-    let cardArray = Array.from(document.getElementsByClassName('card'));
-    console.log(cardArray)
-    
-   //Card shuffling algorithm based on Fisher-Yates shuffle
-    function shuffleCards(cardArray) {
-        for (let i = cardArray.length - 1; i > 0; i--) {
-            let randIndex = Math.floor(Math.random() * (i + 1));
-            cardArray[randIndex].style.order = i;
-            cardArray[i].style.order = randIndex;
-
-            }
-    }    
-
-
- 
-    

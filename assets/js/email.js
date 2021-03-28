9e108da5540ff3ec381ad142cec4c81803b387ef
bubsy07 <bubsy_07@HOTMAIL.CO.UK>
2021-03-28 09:28:25 +0000

    email

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,8 +1,10 @@
+console.log("hello you")
+
 function sendMail(contactForm) {
     emailjs.send("gmail", "remember", {
         "from_name": contactForm.name.value,
         "from_email": contactForm.emailaddress.value,
-        "Thoughts": contactForm.Thoughts.value
+        "Thoughts": contactForm.Remember.value
     })
     .then(
         function(response) {
